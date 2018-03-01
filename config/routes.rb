d76f73504133f74fe1c38ef64c67a34d9bef2c49
Mai Đức Thắng <maiducthang97@gmail.com>
2018-03-01 11:21:46 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,24 @@ Rails.application.routes.draw do
   resources :categories
 
   resources :users do
-    resources :posts, only: [:edit, :update, :destory]  do
-      resources :images,only: [:edit, :update, :show, :destory]
+    resources :posts, only: [:create, :edit, :update, :destory]  do
+      resources :images,only: [:create, :edit, :update, :show, :destory]
     end
   end
 
-  resources :posts, except: [:edit, :update, :destory]
+  resources :posts, except: [:create, :edit, :update, :destory]
 
-  resources :images, except: [:edit, :update, :show, :destory]
+  resources :images, except: [:create, :edit, :update, :show, :destory]
 
   resources :cities do
-    resources :districts, only: [:edit, :update, :show, :destory] do
-      resources :wards, only: [:edit, :update, :show, :destory] do
-        resources :streets, only: [:edit, :update, :show, :destory]
+    resources :districts, only: [:create, :edit, :update, :show, :destory] do
+      resources :wards, only: [:create, :edit, :update, :show, :destory] do
+        resources :streets, only: [:create, :edit, :update, :show, :destory]
       end
     end
   end
-  resources :districts, except: [:edit, :update, :show, :destory]
-  resources :wards, except: [:edit, :update, :show, :destory]
-  resources :streets, except: [:edit, :update, :show, :destory]
+  resources :districts, except: [:create, :edit, :update, :show, :destory]
+  resources :wards, except: [:create, :edit, :update, :show, :destory]
+  resources :streets, except: [:create, :edit, :update, :show, :destory]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
