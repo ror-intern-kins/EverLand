63673fb30bac03cb62f8ccf3242c66e01db7be3d
Yen Minh <minhhy2897@gmail.com>
2018-03-01 16:45:19 +0700

    update validate exists key

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :images
 
+    validate :category_id_exists
+    validate :city_id_exists
+    validate :district_id_exists
+    
     #Attributes can not be blank
     validates :title, :description, :category_id, :city_id, :district_id, :address_number, :contact_mobile, 
         presence: true
@@ -24,5 +28,30 @@ class Post < ApplicationRecord
     validates :contact_name, allow_blank:true ,length: {maximum:200}, format: {with: /\A[a-zA-Z]+\z?/}
     validates :contact_mail, 
         allow_blank:true, length: {maximum: 100}, format: { with:  /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i}
-        
+    
+    protected
+    def category_id_exists
+        begin
+          Category.find(self.category_id)
+        rescue ActiveRecord::RecordNotFound
+          errors.add(:category_id, "category_id foreign key must exist")
+          false
+        end
+    end
+    def city_id_exists
+        begin
+          City.find(self.city_id)
+        rescue ActiveRecord::RecordNotFound
+          errors.add(:city_id, "city_id foreign key must exist")
+          false
+        end
+    end
+    def district_id_exists
+        begin
+          District.find(self.district_id)
+        rescue ActiveRecord::RecordNotFound
+          errors.add(:district_id, "district_id foreign key must exist")
+          false
+        end
+    end
 end
